ccdbd90f33d9dc54f1867c2a5d3b1462ef3f5742
SanukaImodani <sanukaimodani98@gmail.com>
2024-05-09 19:31:12 +0530

    create project

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Handle the login logic here
+    }
+}
