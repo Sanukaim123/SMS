df61376883e40409b5d564d119f2984299ae44fa
SanukaImodani <sanukaimodani98@gmail.com>
2024-06-29 10:08:58 +0530

    front end
    submission form and upload assignment form

--- resources/views/student/submissions/create.blade.php
+++ resources/views/student/submissions/create.blade.php
@@ -5,12 +5,14 @@
 @section('content')
 <div class="container">
     <h1>Submit Assignment for {{ $assignment->course->course_name }}</h1>
+    <br><br>
     <form action="{{ route('student.submissions.store', $assignment->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div>
             <label for="file">Upload File:</label>
             <input type="file" name="file" id="file" required>
         </div>
+        <br><br>
         <button type="submit">Submit Assignment</button>
     </form>
 </div>
--- resources/views/teacher/assignments/create.blade.php
+++ resources/views/teacher/assignments/create.blade.php
@@ -3,24 +3,40 @@
 @section('content')
 <div class="container">
     <h1>Add Assignment</h1>
+    <br>
     <form action="{{ route('teacher.assignments.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div>
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title" required>
-        </div>
-        <div>
-            <label for="description">Description:</label>
-            <textarea name="description" id="description"></textarea>
-        </div>
-        <div>
-            <label for="due_date">Due Date:</label>
-            <input type="date" name="due_date" id="due_date" required>
-        </div>
-        <div>
-            <label for="file">Upload File:</label>
-            <input type="file" name="file" id="file" required>
-        </div>
+        <table>
+            <tr>
+                <div>
+                    <td><label for="title">Title:</label></td>
+                    <td><input type="text" name="title" id="title" required></td>
+                </div>
+            </tr>
+
+            <tr>
+                <div>
+                    <td><label for="description">Description:</label></td>
+                    <td><textarea name="description" id="description"></textarea></td>
+                </div>
+            </tr>
+
+            <tr>
+                <div>
+                  <td><label for="due_date">Due Date:</label></td>
+                  <td><input type="date" name="due_date" id="due_date" required></td>
+                </div>
+            </tr>
+
+            <tr>
+                <div>
+                    <td><label for="file">Upload File:</label></td>
+                    <td>  <input type="file" name="file" id="file" required></td>
+                </div>
+            </tr>
+       </table>
+
+       <br><br>
         <input type="hidden" name="course_code" value="{{ $course_code }}">
         <button type="submit">Add Assignment</button>
     </form>
